8e2267ea152f36e86f4eb032edbd8796f5de2703
alex-romanov <alejandro@mintitmedia.com>
2016-05-20 12:18:16 -0700

    footer

--- src/shared/components/layout/footer/footer1/index.js
+++ src/shared/components/layout/footer/footer1/index.js
@@ -1,6 +1,5 @@
 /* eslint max-len: [2, 500, 4] */
 import React from 'react';
-
 const style = require('./style.scss');
 import Brand from './brand';
 import Contact from './contact';
@@ -8,8 +7,26 @@ import Projects from './projects';
 import About from './about';
 
 export default class Footer1 extends React.Component {
+  componentDidMount() {
+    $('#footerReveal').click(function () {
+      $('#footer_section').addClass(style.fixedFooter);
+      const fHeight = $(window).height();
+      $('#footer_section').height(fHeight);
+      $(this).css('display', 'none');
+    });
+    $('#footer_closer').click(function () {
+      $('#footer_section').removeClass(style.fixedFooter);
+      $('#footer_section').height();
+      $('#footerReveal').css('display', 'block');
+      $('#footer_closer').css('display', 'none');
+    });
+  }
   render() {
     return (<div className={style.footerWrapper} id="footer_section">
+      <div className={style.removeFixed} id="footer_closer">
+        <span className="glyphicon glyphicon-remove"></span>
+      </div>
+      <div id="footerReveal" className={style.showFooterBtn}><span>i</span></div>
       <div className="container-fluid">
         <div className="row">
           <div className="col-xs-12 col-sm-5">
