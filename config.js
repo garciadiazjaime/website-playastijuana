abf015b40b510b988cbea67d5861cf343233d5cf
jaime <garciadiazjaime@gmail.com>
2018-01-15 18:04:19 -0600

    use const

--- config.js
+++ config.js
@@ -1,29 +1,29 @@
-var convict = require('convict');
+const convict = require('convict');
 
-var config = convict({
-    ipaddress: {
-      doc: 'IP the application runs on',
-      format: 'ipaddress',
-      default: '0.0.0.0',
+const config = convict({
+  ipaddress: {
+    doc: 'IP the application runs on',
+    format: 'ipaddress',
+    default: '0.0.0.0',
+  },
+  port: {
+    doc: 'Port the application listens on',
+    format: 'port',
+    default: '3000',
+  },
+  api: {
+    url: {
+      doc: 'API URL',
+      format: String,
+      default: 'http://127.0.0.1:3000/',
+      env: 'API_URL',
     },
-    port: {
-        doc: 'Port the application listens on',
-        format: 'port',
-        default: '3076',
-    },
-    api: {
-      url: {
-          doc: 'API URL',
-          format: String,
-          default: 'http://127.0.0.1:3000/',
-          env: 'API_URL',
-      },
-    },
-    cacheExpiresMins: {
-      doc: 'Number of minutes before query API',
-      format: 'int',
-      default: 30,
-    },
+  },
+  cacheExpiresMins: {
+    doc: 'Number of minutes before query API',
+    format: 'int',
+    default: 30,
+  },
 });
 
 config.validate();
